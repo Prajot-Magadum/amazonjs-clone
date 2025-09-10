6842e03f1d4a8852b88fdc61c20e51f1c5b9e5f5
Prajot-Magadum <prajotmagadum0060@gmail.com>
2025-09-10 17:14:51 +0530

    fix price formating

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -3,7 +3,7 @@
 // to genrate html we can looop throuh each element by array
 import { cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
-
+import { formatCurrency } from './utils/money.js';
 // impoert anthor ways is:: 
 //import * as cartModule from '../data/cart.js';
 //cartModule.cart
@@ -33,7 +33,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-price">
-            $${(product.priceCents /100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
