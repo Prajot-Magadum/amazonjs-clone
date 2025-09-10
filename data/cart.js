64295031df6db5d387a48efbbe15b2a93ef77f03
Prajot-Magadum <prajotmagadum0060@gmail.com>
2025-09-10 19:08:47 +0530

    created local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart=[{
+export let cart=JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart =[{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
 },{
@@ -6,6 +8,12 @@ export let cart=[{
   quantity:1
 }];
 
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 export  function addToCart(productId){
   let matchingItem;
 
@@ -23,6 +31,7 @@ export  function addToCart(productId){
             quantity:1
           });
         }
+        saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -33,4 +42,6 @@ export function removeFromCart(productId){
     }
   });
   cart = newCart;
+
+  saveToStorage();
 }
