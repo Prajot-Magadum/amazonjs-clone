b00f20fd91b73d73e85be99b5218dc5797347a3c
Prajot-Magadum <prajotmagadum0060@gmail.com>
2025-09-12 15:22:54 +0530

    added delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,14 @@
 import { cart,removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+const today = dayjs();
+import { deliveryOptions } from '../data/deliveryOptions.js';
+
+
+
+const deliveryDate = today.add(7,'days');
+console.log(deliveryDate.format('dddd, MMMM D'));
 
 let cartSummaryHTML = '';
 
@@ -94,7 +102,34 @@ cart.forEach((cartItem)=>{
           </div>
           `;
 });
+const priceString = deliveryOption.priceCents(
 
+)
+
+function deliveryOptionsHTML(){
+  deliveryOptions.forEach((deliveryOption)=>{
+    const today = dayjs();
+      const deliveryDate = today.add(
+        deliveryOption.deliveryDays,
+        'days'
+      );
+      const dateString = deliveryDate.format('dddd, MMMM D');
+    `<div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProduct.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      ${dateString}
+                    </div>
+                    <div class="delivery-option-price">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+    `;
+  });
+}
 
 document.querySelector('.js-order-summary')
     .innerHTML = cartSummaryHTML;
